75cc1f8f3579f8fd3b2c14b559fd7c6f9d9e4384
Liors99 <47369500+Liors99@users.noreply.github.com>
2020-11-02 10:08:25 -0700

    Changed app UI, added user list, prepared for user color change

--- server/index.js
+++ server/index.js
@@ -2,13 +2,53 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+//Dictiomary with username: {color: }
+let all_active_users = {};
+
+//Generates a random 5 digit ID
+const generateUserId = () => {
+    let id;
+    let username;
+    do {
+        id = Math.floor(Math.random() * 90000) + 10000;
+        username = "user-" + id
+    } while (username in all_active_users);
+    return username;
+}
+
 
 io.on("connection", (socket) => {
-    socket.emit('set username', "user-" + socket.id);
+    //TODO: Check cookie and assign previous username
+    const username = generateUserId();
+
+    //Assign username
+    socket.emit('set username', username);
+    //Notify all connected users of the newly connected user
+    io.emit('user connect', username);
+
+    //Let the new connection know of all the existing users
+    for (const connected_user in all_active_users) {
+        socket.emit('user connect', connected_user);
+    }
+
+    //Add the user to all the users list
+    //all_active_users.push(username);
+    all_active_users[username] = { "color": "rgb(0, 0, 0)" };
+
+    //Handle disconnect
+    socket.on('disconnect', () => {
+        delete all_active_users[username];
+        io.emit('user disconnect', username);
+    });
+
+
     socket.on("message", ({ name, message }) => {
-        io.emit("message", { name, message })
+        const user_obj = all_active_users[name];
+        io.emit("message", { name, user_obj, message, })
     });
 });
+
+
 
 http.listen(4000, () => {
     console.log("Listening on port 4000");
--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client'
-import TextField from '@material-ui/core/TextField'
+import io from 'socket.io-client';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import SendIcon from '@material-ui/icons/Send';
 
 import './App.css';
 
@@ -10,11 +13,25 @@ function App() {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+  const [activeUsers, setActiveUsers] = useState([]);
 
 
   useEffect(() => {
-    socket.on('message', ({ name, message }) => {
-      setChat(oldChat => [...oldChat, { name, message }]);
+
+    //Handle new user
+    socket.on('user connect', (username) => {
+      console.log("Connected user: " + username);
+      setActiveUsers(oldUsers => [...oldUsers, username]);
+    });
+
+    //Handle user disconnect
+    socket.on('user disconnect', (username) => {
+      console.log("Disconnected user: " + username);
+      setActiveUsers(oldUsers => oldUsers.filter(val => val !== username));
+    });
+
+    socket.on('message', ({ name, user_obj, message }) => {
+      setChat(oldChat => [...oldChat, { name, user_obj, message }]);
     });
 
     socket.on('set username', (username) => {
@@ -29,37 +46,98 @@ function App() {
   const onMessageSubmit = (e) => {
     e.preventDefault();
     socket.emit('message', { name, message });
+    setMessage("");
+  }
+
+  const renderActiveUserList = () => {
+    return activeUsers.map(username => (
+      <Grid item xs={12}>
+        <h2>{username} {username === name ? "(You)" : ""}</h2>
+      </Grid>
+    ))
+
+
   }
 
   const renderChat = () => {
-    return chat.map(({ name, message }, index) => (
+    return chat.map(({ name, user_obj, message }, index) => (
       <div key={index}>
         <h3>
-          {name}: <span>{message}</span>
+          {name}: <span style={{ "color": user_obj["color"] }}>{message}</span>
         </h3>
       </div>
     ))
   }
 
   return (
-    <div>
-      {renderChat()}
-      <form onSubmit={onMessageSubmit}>
-        <div>
-          <TextField
-            name="message"
-            onChange={e => onTextChange(e)}
-            value={message}
-            label="Message"
-            //Material UI stuff
-            id="outlined-multiline-static"
-            variant="outlined"
-          />
-          <button type="submit"> Send Message </button>
-        </div>
+    <Grid
+      container
+      spacing={0}
+      id="chat-container"
+    >
+      <Grid item xs={3} id="user-list">
+        <h1> Online Users </h1>
+        <Grid container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          {renderActiveUserList()}
+        </Grid>
+      </Grid>
 
-      </form>
-    </div>
+      <Grid item xs={9}>
+        <Grid container id="chat-and-msg">
+          <Grid item xs={12} id="chat-window">
+            {renderChat()}
+
+          </Grid>
+
+
+          <Grid item xs={12} id="new-message-container">
+
+            {
+              <form onSubmit={onMessageSubmit} style={{ height: "100%" }}>
+                <Grid container spacing={0} style={{ height: "100%" }}>
+                  <Grid item xs={10}>
+                    <TextField
+                      name="message"
+                      onChange={e => onTextChange(e)}
+                      value={message}
+                      label="Message"
+                      rows={1}
+                      multiline
+                      fullWidth
+                      className={"text-area"}
+
+
+                    />
+
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      endIcon={<SendIcon />}
+                      type="submit"
+                      fullWidth
+                      className={"text-area"}
+                    >
+                      Send
+                  </Button>
+
+                  </Grid>
+
+                </Grid>
+              </form>
+            }
+          </Grid>
+        </Grid>
+      </Grid>
+
+
+
+    </Grid>
   );
 }
 
